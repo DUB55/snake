b8694b8ee54902abd1d2c45ccb933dc82dfd5f23
DUB55 <tcookdub5@gmail.com>
2025-01-06 19:24:26 +0100

    Update thefile.js

--- thefile.js
+++ thefile.js
@@ -55,13 +55,9 @@ window.onload = function () {
                         textElement.style.color = 'white';
                         textElement.style.fontWeight = 'bold';
                     }
-                }
-            })();
 
-            // Hide elements #mod-descriptions > div:nth-child(x) > span:nth-child(3) for x=1 to 10
-            (function () {
-                for (let i = 1; i <= 10; i++) {
-                    const elementToHide = document.querySelector(`#mod-descriptions > div:nth-child(${i}) > span:nth-child(3)`);
+                    // Hide elements #mod-descriptions > div:nth-child(x) > span:nth-child(5) for x=1 to 10
+                    const elementToHide = document.querySelector(`#mod-descriptions > div:nth-child(${i}) > span:nth-child(5)`);
                     if (elementToHide) {
                         elementToHide.style.display = 'none';
                     }
@@ -80,6 +76,78 @@ window.onload = function () {
                     applyModButton.textContent = 'Spelen';
                 }
             })();
+
+            // Hide specific images
+            (function () {
+                const imgSelectors = [
+                    '#mod-loader-title > span:nth-child(1) > a > img',
+                    '#mod-loader-title > span:nth-child(3) > a > img'
+                ];
+                imgSelectors.forEach(selector => {
+                    const imgElement = document.querySelector(selector);
+                    if (imgElement) {
+                        imgElement.style.display = 'none';
+                    }
+                });
+            })();
+
+            // Hide elements based on the provided selectors
+            (function () {
+                const selectorsToHide = [
+                    '#mod-descriptions > div:nth-child(10) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(9) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(8) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(7) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(6) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(5) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(4) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(3) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(2) > span:nth-child(5)',
+                    '#mod-descriptions > div:nth-child(1) > span:nth-child(5)',
+                    '#mod-game-verson-div > label',
+                    '#mod-game-version',
+                    '#mod-game-verson-div > a',
+                    '#version-info',
+                    '#advanced-options-show-settings-text',
+                    '#advanced-options-toggle'
+                ];
+
+                selectorsToHide.forEach(selector => {
+                    const element = document.querySelector(selector);
+                    if (element) {
+                        element.style.display = 'none';
+                    }
+                });
+            })();
+
+            // Apply gradient to a specific element
+            (function () {
+                function applyGradient(selector, gradient) {
+                    const element = document.querySelector(selector);
+                    if (element) {
+                        element.style.background = gradient;
+                        console.log(`Applied gradient to: ${selector}`);
+                    } else {
+                        console.log(`Element with selector ${selector} not found`);
+                    }
+                }
+
+                applyGradient('#mod-selector-dialogue', 'linear-gradient(to top, #000033, #00008B)');
+            })();
+
+            // Function to style elements (change color and font weight)
+            function styleElement(selector) {
+                const element = document.querySelector(selector);
+                if (element) {
+                    element.style.color = 'white';
+                    element.style.fontWeight = 'bold';
+                }
+            }
+
+            // Apply styles to 11 selectors
+            for (let i = 1; i <= 11; i++) {
+                styleElement(`#mod-options > label:nth-child(${i})`);
+            }
 
             console.log('Script executed successfully.');
         });
